07bfc5e03fc81a4983ec636a1c3a63f72b98e114
anastasia-chaplits <anastasia.chaplits@contractors.medallia.com>
2021-07-09 09:22:45 +0300

    Annotate dispatch

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
+import { Dispatch } from 'redux';
 import { Action } from './../actions/index';
 import { ActionType } from '../action-types';
 
 export const searchRepositories = (term: string) => {
-  return async (dispatch: any) => {
+  return async (dispatch: Dispatch<Action>) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES
     });
